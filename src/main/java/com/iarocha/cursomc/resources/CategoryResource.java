74f9e993cad2119865b3152084634eb648a52181
Ian Rocha <ianbritosr@gmail.com>
2018-10-04 14:18:05 -0300

    create first domain class - category

--- src/main/java/com/iarocha/cursomc/resources/CategoryResource.java
+++ src/main/java/com/iarocha/cursomc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.iarocha.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.iarocha.cursomcm.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest is working!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Technology");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> categoryList = new ArrayList<>();
+		categoryList.add(cat1);
+		categoryList.add(cat2);
+		
+		return categoryList;
 	}
 
 }
