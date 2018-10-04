218204687a7c6a319589fe98655b7a393d5dd256
Ian Rocha <ianbritosr@gmail.com>
2018-10-04 16:09:53 -0300

    Add instance operation for Category table

--- src/main/java/com/iarocha/cursomc/CursomcApplication.java
+++ src/main/java/com/iarocha/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.iarocha.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.iarocha.cursomc.domain.Category;
+import com.iarocha.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Technology");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
